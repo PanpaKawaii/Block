380dad822269c1b5bf1408f0b7690766e48fbca1
PanpaKawaii <dngngchitriu2004@gmail.com>
2026-09-11 00:56:13 +0700

    Fix VectorControllerPanel

--- src/app/pages/GenerateObject/VectorControllerPanel/VectorControllerPanel.jsx
+++ src/app/pages/GenerateObject/VectorControllerPanel/VectorControllerPanel.jsx
@@ -1,24 +1,22 @@
 import { useState } from 'react';
-import ButtonList from '../../../components/ButtonList/ButtonList.jsx';
-import CopyPasteButton from '../../../components/CopyPasteButton/CopyPasteButton.jsx';
+import ControlPanel from '../../../components/ControlPanel/ControlPanel.jsx';
 import MovingLabelInput from '../../../components/MovingLabelInput/MovingLabelInput.jsx';
 import StyleLabelSelect from '../../../components/StyleLabelSelect/StyleLabelSelect.jsx';
 import './VectorControllerPanel.css';
 
 export default function VectorControllerPanel({
-    dots,
-    setFaces,
-    selectedFace,
-    vectors,
-    setVectors,
-    selectedVectorId,
-    setSelectedVectorId,
-    selectedVector,
-    toggleMenu,
-    toggleStepFunction,
-    collapseController,
-    swapController,
-    hexRgbaToPercent
+    selectedFace = null,
+    dots = [],
+    vectors = [],
+    setVectors = () => { },
+    selectedVectorId = '',
+    setSelectedVectorId = () => { },
+    selectedVector = null,
+    toggleMenu = false,
+    toggleStepFunction = '',
+    collapseController = () => { },
+    swapController = () => { },
+    hexRgbaToPercent = () => { }
 }) {
     const [openedVectorId, setOpenedVectorId] = useState([]);
     const toggleOpenVector = (vectorId) => {
@@ -102,7 +100,7 @@ export default function VectorControllerPanel({
                 vector.id === vectorId
                     ? {
                         ...vector,
-                        name: `${index == 1 ? name + '-' + vector.name?.split('-')?.[1] : vector.name?.split('-')?.[0] + '-' + name} (${xB - xA},${yB - yA},${zB - zA})`,
+                        name: `${index == 1 ? name + '-' + vector.name?.split('-')?.[1]?.split(' ')?.[0] : vector.name?.split('-')?.[0] + '-' + name} (${xB - xA},${yB - yA},${zB - zA})`,
                         xCoordinateA: xA,
                         yCoordinateA: yA,
                         zCoordinateA: zA,
@@ -116,21 +114,17 @@ export default function VectorControllerPanel({
     };
 
     return (
-        <div className={`vector-controller-panel-container face-dot-vector-function-controller-container card ${toggleMenu ? '' : 'collapsed'} ${toggleStepFunction == 'vector' ? (selectedFace ? 'size_1_2' : 'size_1_1') : (selectedFace ? 'size_1_4' : 'size_1_3')}`}>
-            <div className='heading'>
-                <h2>Vector Ctrler</h2>
-                <div className='control'>
-                    <button className='btn btn-collapsed' onClick={collapseController}><i className='fa-solid fa-chevron-right' /></button>
-                    <CopyPasteButton data={vectors} setData={setVectors} />
-                    <button className='btn' onClick={addVector}><i className='fa-solid fa-plus' /></button>
-                    <button className='btn btn-remove' onClick={() => setVectors([])}><i className='fa-solid fa-trash-can' /></button>
-                    <ButtonList
-                        icon={'arrow-right-arrow-left'}
-                        onToggle={swapController}
-                    />
-                </div>
-            </div>
-
+        <ControlPanel
+            titleName={'Vector Controller'}
+            extraClassName={'vector-controller-panel-container'}
+            collapseMenu={toggleMenu}
+            size={toggleStepFunction == 'vector' ? (selectedFace ? 'size_1_2' : 'size_1_1') : (selectedFace ? 'size_1_4' : 'size_1_3')}
+            collapseController={collapseController}
+            entity={vectors}
+            setEntity={setVectors}
+            addEntity={addVector}
+            swapController={swapController}
+        >
             <div className='list'>
                 {vectors.map((vector) => (
                     <div key={vector.id} className={`card ${vector.visible == 0 ? 'invisible' : ''} ${vector.id == selectedVectorId ? 'dash-box' : ''}`}>
@@ -142,15 +136,17 @@ export default function VectorControllerPanel({
                                 className='input color-input'
                                 style={{ opacity: hexRgbaToPercent(vector.color || '#FFFFFFFF') || 1 }}
                             />
-                            <MovingLabelInput
-                                type={'text'}
-                                value={vector?.name || ''}
-                                onValueChange={(propE) => updateVector(vector?.id, 'name', propE.value)}
-                                extraClassName={''}
-                                extraStyle={{}}
-                                label={'Name'}
-                                labelStyle={'left moving'}
-                            />
+                            <div className={`name-group ${(openedVectorId.includes(vector.id) || !toggleMenu) ? 'expanse' : ''}`}>
+                                <MovingLabelInput
+                                    type={'text'}
+                                    value={vector?.name || ''}
+                                    onValueChange={(propE) => updateVector(vector?.id, 'name', propE.value)}
+                                    extraClassName={''}
+                                    extraStyle={{}}
+                                    label={'Name'}
+                                    labelStyle={'left moving'}
+                                />
+                            </div>
                             <div className='btns'>
                                 <button className={`btn-click ${selectedVectorId == vector.id ? 'selected' : ''}`} onClick={() => toggleSelectVector(vector.id)}><i className='fa-solid fa-gear' /></button>
                                 <button className={`btn-click ${openedVectorId.includes(vector.id) ? 'opened-select' : ''}`} onClick={() => toggleOpenVector(vector.id)}><i className='fa-solid fa-hand' /></button>
@@ -252,6 +248,6 @@ export default function VectorControllerPanel({
                     </div>
                 ))}
             </div>
-        </div>
+        </ControlPanel>
     )
 }
